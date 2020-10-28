8d3cd19d6dabf836a79c06468086e6194a82c42f
Madhura <madhura.bhujbal@gmail.com>
2020-10-28 14:54:03 +0000

    Write a simple code for performing operations using functions

--- Promise.js
+++ Promise.js
@@ -1,14 +1,39 @@
-let p = new Promise((resolve, reject) => {
-    let a = 1 + 1;
-    if (a == 2) {
-        resolve("Success");
-    } else {
-        reject("Failed");
-    }
-});
+// let p = new Promise((resolve, reject) => {
+//     let a = 1 + 1;
+//     if (a == 2) {
+//         resolve("Success");
+//     } else {
+//         reject("Failed");
+//     }
+// });
 
-p.then((message) => {
-    console.log("In then with message: " + message);
-}).catch((message) => {
-    console.log("In catch with message: " + message);
+// p.then((message) => {
+//     console.log("In then with message: " + message);
+// }).catch((message) => {
+//     console.log("In catch with message: " + message);
+// });
+
+const userLeft = false;
+const userWatchingCatMeme = false;
+
+function watchTutorialCallBack(callback, errorCallback) {
+    if (userLeft) {
+        errorCallback({
+            name: "User left",
+            message: ":("
+        });
+    } else if (userWatchingCatMeme) {
+        errorCallback({
+            name: "User watching cat memes",
+            message: "Something < cat"
+        });
+    } else {
+        callback("Thumbs up and subscribe");
+    }
+}
+
+watchTutorialCallBack((message) => {
+    console.log("Success: " + message);
+}, (error) => {
+    console.log(error.name + " " + error.message);
 });
