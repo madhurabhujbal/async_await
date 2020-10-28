c149e33d27e0bc73a9ea247e3f6aa614d64a78ec
Madhura <madhura.bhujbal@gmail.com>
2020-10-28 14:27:28 +0000

    Write example code for understanding Promise

---
+++ Promise.js
@@ -0,0 +1,14 @@
+let p = new Promise((resolve, reject) => {
+    let a = 1 + 1;
+    if (a == 2) {
+        resolve("Success");
+    } else {
+        reject("Failed");
+    }
+});
+
+p.then((message) => {
+    console.log("In then with message: " + message);
+}).catch((message) => {
+    console.log("In catch with message: " + message);
+});
