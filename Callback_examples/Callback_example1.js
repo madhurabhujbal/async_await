9bf5157be3ebc89afbaedd17768e1068cf0a63e2
Madhura <madhura.bhujbal@gmail.com>
2020-10-29 14:47:05 +0000

    Write example code of a named function and its equivalent arrow function

--- Callback_examples/Callback_example1.js
+++ Callback_examples/Callback_example1.js
@@ -1,11 +1,19 @@
 //A function that accepts another function as an argument, is called a callback function
 
-setTimeout(() => {
-    console.log("Coffee");
-}, 3000);
-console.log("Tea");
-console.log("Eggs");
+// setTimeout(() => {
+//     console.log("Coffee");
+// }, 3000);
+// console.log("Tea");
+// console.log("Eggs");
 
-const names = ['John', 'Mary', 'Bob'];
-const greet = names.map(name => `Hello ${name}`);
-console.log(greet);
+// const names = ['John', 'Mary', 'Bob'];
+// const greet = names.map(name => `Hello ${name}`);
+// console.log(greet);
+
+// function greeting(name) {
+//     console.log(`Hello ${name}`);
+// }
+
+let greeting = name => console.log(`Hello ${name}!`);
+
+greeting('John');
