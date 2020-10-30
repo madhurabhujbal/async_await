bdfb1061151e2e54984f59f1a3ef66b949abbe47
Madhura <madhura.bhujbal@gmail.com>
2020-10-30 11:35:35 +0000

    Write addToCalender Promise in an easier and short way

--- Promise_examples/Promise_example4.js
+++ Promise_examples/Promise_example4.js
@@ -12,13 +12,16 @@ const meeting = new Promise((resolve, reject) => {
     }
 });
 
+// const addToCalender = meetingDetails => {
+//     return new Promise((resolve, reject) => {
+//         const calender = `${meetingDetails.name} is scheduled at ${meetingDetails.time} on ${meetingDetails.location}`;
+//         resolve(calender);
+//     });
+// }
 const addToCalender = meetingDetails => {
-    return new Promise((resolve, reject) => {
         const calender = `${meetingDetails.name} is scheduled at ${meetingDetails.time} on ${meetingDetails.location}`;
-        resolve(calender);
-    });
+       return new Promise.resolve(calender);
 }
-
 
 meeting
 .then(res => {
