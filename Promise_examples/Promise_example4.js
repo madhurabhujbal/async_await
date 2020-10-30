15cbf837b0e28e5f8ef44b65633df8710fe6c94a
Madhura <madhura.bhujbal@gmail.com>
2020-10-30 11:33:51 +0000

    Add one more promise addToCalender to the code

--- Promise_examples/Promise_example4.js
+++ Promise_examples/Promise_example4.js
@@ -12,6 +12,14 @@ const meeting = new Promise((resolve, reject) => {
     }
 });
 
+const addToCalender = meetingDetails => {
+    return new Promise((resolve, reject) => {
+        const calender = `${meetingDetails.name} is scheduled at ${meetingDetails.time} on ${meetingDetails.location}`;
+        resolve(calender);
+    });
+}
+
+
 meeting
 .then(res => {
     console.log('Meeting scheduled');
