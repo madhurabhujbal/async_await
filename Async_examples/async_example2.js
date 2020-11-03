5d45182fcd8be8adf78f30c42459ede55f582371
Madhura <madhura.bhujbal@gmail.com>
2020-11-03 17:11:41 +0000

    Add synchronous code for practice

---
+++ Async_examples/async_example2.js
@@ -0,0 +1,10 @@
+//Sync code example
+
+function otherfunction() {
+    console.log("we are in other function");
+    console.log("do some stuff");
+}
+
+console.log("Start");
+otherfunction();
+console.log("End");
