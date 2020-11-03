682dca1676ae18f4f505ce2cc80d237542c51a61
Madhura <madhura.bhujbal@gmail.com>
2020-11-03 18:55:13 +0000

    Write code using async setTimeout function

--- Async_examples/async_example2.js
+++ Async_examples/async_example2.js
@@ -1,10 +1,19 @@
-//Sync code example
+// //Sync code example
 
-function otherfunction() {
-    console.log("we are in other function");
-    console.log("do some stuff");
-}
+// function otherfunction() {
+//     console.log("we are in other function");
+//     console.log("do some stuff");
+// }
 
+// console.log("Start");
+// otherfunction();
+// console.log("End");
+
+//Async code example
 console.log("Start");
-otherfunction();
+
+setTimeout(() => {
+    console.log("We are in a timeout");
+}, 2000);
+
 console.log("End");
